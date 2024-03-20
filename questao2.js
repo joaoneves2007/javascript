5fa644c9af77590c646e669ce1ca45fc4b0e1635
João Neves <contatojotaico@gmail.com>
2024-03-20 10:45:35 -0400

    finalizado questao 2

---
+++ questao2.js
@@ -0,0 +1,9 @@
+/*
+Questão 2: Como você pode verificar o tipo de uma variável em JavaScript?
+*/
+
+let variavel = 10;
+console.log(typeof variavel); // Isso imprimirá 'number' no console
+
+variavel = "Olá";
+console.log(typeof variavel); // Isso imprimirá 'string' no console
