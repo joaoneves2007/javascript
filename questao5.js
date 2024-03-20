4d92cf0c853da19ff08747cccd5c54f938b3057e
João Neves <contatojotaico@gmail.com>
2024-03-20 10:46:55 -0400

    finalizado questao 5

---
+++ questao5.js
@@ -0,0 +1,10 @@
+/*
+Questão 5: Como você pode criar uma função em JavaScript?
+*/
+
+function minhaFuncao() {
+    console.log("Esta é uma função em JavaScript.");
+}
+
+// Chamando a função
+minhaFuncao();
