b32009507069d2210da73ed6eb98cbdd6e8f3f6b
João Neves <contatojotaico@gmail.com>
2024-03-20 10:50:33 -0400

    finalizado questao 14

---
+++ questao14.js
@@ -0,0 +1,8 @@
+/*
+Questão 14: Como você pode converter uma string em minúsculas em 
+JavaScript?
+*/
+
+let minhaString = "Exemplo de STRING";
+let stringMinuscula = minhaString.toLowerCase();
+console.log(stringMinuscula); // Isso imprimirá "exemplo de string" no console
