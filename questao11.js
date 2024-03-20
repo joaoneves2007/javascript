68fb639dfadb33a7403f574006323d65c5541656
João Neves <contatojotaico@gmail.com>
2024-03-20 10:49:43 -0400

    finalizado questao 11

---
+++ questao11.js
@@ -0,0 +1,6 @@
+/*
+Questão 11: Qual é a diferença entre let, const e var em JavaScript?
+
+Em resumo, var é mais antigo e tem um escopo menos previsível, enquanto let e const são mais modernos, com let permitindo reatribuições e const fornecendo imutabilidade após a atribuição inicial. O uso de const é incentivado sempre que você tiver uma variável cujo valor não precisa mudar.
+*/
+
