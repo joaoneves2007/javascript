fa3bab8f9742b18f28adec68d37d712ec6191fab
João Neves <contatojotaico@gmail.com>
2024-03-20 10:54:22 -0400

    finalizado questao 29

---
+++ questao29.js
@@ -0,0 +1,8 @@
+/*
+Questão 29: Como você pode converter um número em uma string em
+JavaScript?
+*/
+
+let meuNumero = 123;
+let minhaString = meuNumero.toString();
+console.log(minhaString); // Isso imprimirá "123" no console
