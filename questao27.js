2ab6cd67be69461190a51dc74cb3b440c55925e1
João Neves <contatojotaico@gmail.com>
2024-03-20 10:53:53 -0400

    finalizado questao 27

---
+++ questao27.js
@@ -0,0 +1,8 @@
+/*
+Questão 27: Como você pode remover um elemento específico de um array 
+em JavaScript?
+*/
+
+let meuArray = [1, 2, 3, 4, 5];
+meuArray.splice(2, 1); // Remove o elemento de índice 2 (valor 3)
+console.log(meuArray); // Isso imprimirá [1, 2, 4, 5] no console
