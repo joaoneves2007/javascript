938060956d602e4fafece1acb0907755444eea74
João Neves <contatojotaico@gmail.com>
2024-03-20 10:54:00 -0400

    finalizado questao 28

---
+++ questao28.js
@@ -0,0 +1,8 @@
+/*
+Questão 28: Como você pode converter uma string em um número inteiro em 
+JavaScript?
+*/
+
+let minhaString = "123";
+let meuNumero = parseInt(minhaString);
+console.log(meuNumero); // Isso imprimirá 123 no console
