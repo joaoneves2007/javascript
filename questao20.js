1f59e53609507bace398f2eccc69d4dcd0cc4a5a
João Neves <contatojotaico@gmail.com>
2024-03-20 10:52:09 -0400

    finalizado questao 20

---
+++ questao20.js
@@ -0,0 +1,9 @@
+/*
+Questão 20: Como você pode chamar uma função de retorno de chamada em 
+JavaScript?
+*/
+
+function minhaCallback(parametro) {
+    console.log("Esta é a função de retorno de chamada.");
+    console.log("O parâmetro recebido é:", parametro);
+}
