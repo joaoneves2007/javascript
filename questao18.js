ab55e8dc5185c8b640f0909598d596093a51f68c
João Neves <contatojotaico@gmail.com>
2024-03-20 10:51:30 -0400

    finalizado questao 18

---
+++ questao18.js
@@ -0,0 +1,10 @@
+/*
+Questão 18: Como você pode acessar o valor de uma propriedade de um 
+objeto em JavaScript?
+*/
+
+let meuObjeto = {
+    propriedade: "valor"
+};
+
+console.log(meuObjeto.propriedade); // Isso imprimirá "valor" no console
