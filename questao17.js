27b50b19b62b3b3423b07e1d99ba026e76eddca9
João Neves <contatojotaico@gmail.com>
2024-03-20 10:51:19 -0400

    finalizado questao 17

---
+++ questao17.js
@@ -0,0 +1,11 @@
+/*
+Questão 17: Como você pode adicionar uma propriedade a um objeto em 
+JavaScript?
+*/
+
+let meuObjeto = {}; // Objeto vazio
+
+// Adicionando uma propriedade ao objeto
+meuObjeto.propriedade = "valor";
+
+console.log(meuObjeto); // Isso imprimirá { propriedade: "valor" } no console
