035c56f012e0e7da21b987cbf7f87e561b660674
João Neves <contatojotaico@gmail.com>
2024-03-20 11:04:42 -0400

    finalizado tipo de dados

---
+++ tipo-de-dados.js
@@ -0,0 +1,23 @@
+//var nome = "Felipe";
+//console.log(typeof(nome));
+
+//var caracter = 'F';
+//console.log(typeof(caracter));
+//var idade = 25;
+//console.log(typeof(idade));
+
+//var salario = 1.500;
+//console.log(typeof(salario));
+
+//var ligado = false;
+//console.log(typeof(ligado));
+
+//var ligado = true;
+//console.log(typeof(desligada));
+
+var a = 10;
+//console.log(typeof(a));
+var b = "10";
+//console.log(typeof(b));
+
+console.log(a == b);
