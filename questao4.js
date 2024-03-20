88675f5ddc28017cb25eef017d393993f5c9520d
João Neves <contatojotaico@gmail.com>
2024-03-20 10:46:18 -0400

    finalizado questao 4

---
+++ questao4.js
@@ -0,0 +1,7 @@
+/*
+Questão 4: Qual é a diferença entre == e === em JavaScript?
+*/
+
+console.log(5 == "5"); // Isso imprimirá true, porque o valor é o mesmo após a conversão de tipo
+
+console.log(5 === "5"); // Isso imprimirá false, porque os tipos são diferentes (number e string)
