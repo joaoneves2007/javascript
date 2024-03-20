2ee0e6875bdea2d1eecc013c4a679708a2431832
João Neves <contatojotaico@gmail.com>
2024-03-20 10:46:00 -0400

    finalizado questao 3

---
+++ questao3.js
@@ -0,0 +1,8 @@
+/*
+Questão 3: Como você pode concatenar duas strings em JavaScript?
+*/
+
+let string1 = "Olá";
+let string2 = " mundo!";
+let resultado = string1 + string2;
+console.log(resultado); // Isso imprimirá "Olá mundo!" no console
