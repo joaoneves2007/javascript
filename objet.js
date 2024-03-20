eb34ecd64a7c4829d0387a66af7e431ab8b483fe
João Neves <contatojotaico@gmail.com>
2024-03-20 11:03:47 -0400

    finalizado objet

---
+++ objet.js
@@ -0,0 +1,9 @@
+let pessoa = {
+    nome: "João",
+    idade: 30,
+    profissao: "Engenheiro"
+};
+
+console.log(pessoa.nome);        // Saída: João
+console.log(pessoa.idade);       // Saída: 30
+console.log(pessoa.profissao);   // Saída: Engenheiro
