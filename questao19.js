b149a12f508283ca6a1a35108b40fdc681c1138f
João Neves <contatojotaico@gmail.com>
2024-03-20 10:51:49 -0400

    finalizado questao 19

---
+++ questao19.js
@@ -0,0 +1,9 @@
+/*
+Questão 19: Como você pode definir uma função de retorno de chamada 
+(callback) em JavaScript?
+*/
+
+function minhaCallback(parametro) {
+    console.log("Esta é a função de retorno de chamada.");
+    console.log("O parâmetro recebido é:", parametro);
+}
